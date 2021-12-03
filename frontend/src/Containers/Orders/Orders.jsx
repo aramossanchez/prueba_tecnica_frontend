75c82ec0c7541dffd76fefebe8c6cdc48d79c9a6
Armando <armandoramossanchez@gmail.com>
2021-12-03 22:24:41 +0100

    añado botones de editar y borrar y funcionalidad de borrar registro, y mejoras visuales

--- frontend/src/Containers/Orders/Orders.jsx
+++ frontend/src/Containers/Orders/Orders.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import './Orders.scss';
 
+import actualizar from '../../img/actualizar.png';
+import borrar from '../../img/borrar.png';
+
 const Orders = () =>{
 
     //HOOK PARA GUARDAR ORDERS DE BASE DE DATOS
@@ -49,6 +52,43 @@ const Orders = () =>{
         setPosicion(posicion);
     }
 
+    //ACTUALIZAR REGISTRO
+    const actualizarRegistro = async(order) =>{
+        inputNoEditable();
+        try {
+            //TODOS LOS INPUTS DEL REGISTRO QUE QUEREMOS ACTUALIZAR SE VUELVEN EDITABLES
+            let inputs = document.getElementsByName(order.order_id);
+
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].readOnly = false;
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    //HACEMOS QUE NINGÚN INPUT QUE MUESTRA INFORMACIÓN DE LOS REGISTROS SEA EDITABLE
+    const inputNoEditable = () =>{
+        let div = document.getElementsByClassName("listado-orders");
+        for (let i = 0; i < div.length; i++) {
+            console.log(div[i]);            
+        }
+    }
+
+    //BORRAR REGISTRO
+    const borrarRegistro = async(id) =>{
+
+        try {
+            
+            await axios.delete(`http://localhost:4000/orders/eliminiarRegistro/${id}`);
+            traerOrders();
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return(
         <div className="contenedor-orders">
             {/* NOMBRES DE LOS CAMPOSº */}
@@ -60,18 +100,23 @@ const Orders = () =>{
                     <li>COMPANY NAME</li>
                     <li>STATUS</li>
                     <li>TYPE</li>
+                    <li>ACTIONS</li>
                 </ul>
             </div>
             {/* LISTADO DE ORDERS PARA MOSTRAR */}
             {pagina.map((order)=>{
                 return(
                     <div key={order.id} className="listado-orders">
-                        <input type="text" readOnly value={order.order_id}/>
-                        <input type="text" readOnly value={order.country}/>
-                        <input type="text" readOnly value={order.ship_date}/>
-                        <input type="text" readOnly value={order.company_name}/>
-                        <input type="text" readOnly value={order.status}/>
-                        <input type="text" readOnly value={order.type}/>
+                        <input name={order.order_id} type="text" readOnly value={order.order_id}/>
+                        <input name={order.order_id} type="text" readOnly value={order.country}/>
+                        <input name={order.order_id} type="text" readOnly value={order.ship_date}/>
+                        <input name={order.order_id} type="text" readOnly value={order.company_name}/>
+                        <input name={order.order_id} type="text" readOnly value={order.status}/>
+                        <input name={order.order_id} type="text" readOnly value={order.type}/>
+                        <div className="acciones">
+                            <img onClick={()=>actualizarRegistro(order)} src={actualizar} alt="Lapiz Actualizar" />
+                            <img onClick={()=>borrarRegistro(order.id)} src={borrar} alt="Papelera Borrar" />
+                        </div>
                     </div>
                 )
             })}
