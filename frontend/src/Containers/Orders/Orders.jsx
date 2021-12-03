3e512c5c9d0b747c5757209087bc10519e847c82
Armando <armandoramossanchez@gmail.com>
2021-12-03 20:15:04 +0100

    creo container orders funcional

---
+++ frontend/src/Containers/Orders/Orders.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Orders.scss';
+
+const Orders = () =>{
+
+    const traerOrders = () =>{
+        
+    }
+    return(
+        <div>
+            soy orders
+        </div>
+    )
+}
+
+export default Orders;
